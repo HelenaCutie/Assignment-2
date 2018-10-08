44c49dcc82721f5b2d2796c8d27c924438e95bb8
ZmeyGarynich <43955374+ZmeyGarynich@users.noreply.github.com>
2018-10-09 00:55:38 +1100

    Add files via upload

---
+++ inverted.h
@@ -0,0 +1,10 @@
+// Interface to createInvertedIndex function
+// Written by Michael Darmanian 8/10/18
+
+#ifndef INVERTED_H
+#define INVERTED_H
+
+// Outputs the inverted index to file invertedIndex.txt given a set of urls
+void createInvertedIndex(Set urls);
+
+#endif
