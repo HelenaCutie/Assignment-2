9563921693e1664b9ca4f746d6b01569db5312b2
ZmeyGarynich <43955374+ZmeyGarynich@users.noreply.github.com>
2018-10-18 00:11:23 +1100

    Update readData.h

--- readData.h
+++ readData.h
@@ -4,9 +4,17 @@
 #ifndef READDATA_H
 #define READDATA_H
 
+typedef unsigned char Num;
+
+typedef struct GraphRep *Graph;
+
+typedef struct SetRep *Set;
+
 // Creates set of urls from collection.txt
 Set urlList();
 // Creates graph of urls from collection.txt
 Graph urlGraph(Set list);
+// Returns pointer to opened url file
+FILE *openUrl(char *url);
 
 #endif
