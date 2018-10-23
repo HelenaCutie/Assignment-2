59ea8fa8cf68f816b43953d6bb279f721ccb8198
ZmeyGarynich <43955374+ZmeyGarynich@users.noreply.github.com>
2018-10-24 00:47:56 +1100

    Update readData.c

--- readData.c
+++ readData.c
@@ -2,12 +2,13 @@
 // Written by Helena Ling on 07/10/18
 // Acknowledgement: Graph, stack, queue, set ADTs written by John Shepherd, BSTree ADT from week10 lab
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
-#include "stack.h"
 #include "set.h"
 #include "graph.h"
 #include "readData.h"
@@ -39,7 +40,7 @@ Graph urlGraph(Set list) {
                 break;
             }
             char *search = strstr(dest, "url");
-            if (search != NULL) {
+            if (strcmp(curr->val, dest) != 0 && search != NULL) {
                 addEdge(g, curr->val, dest);
             }
         }
@@ -49,7 +50,8 @@ Graph urlGraph(Set list) {
     return g;
 }
 
-FILE *openUrl(char *url) {
+FILE *openUrl(char *url)
+{
     FILE *fp;
     char filename[MAXURL];
     strcpy(filename, url);
@@ -60,4 +62,60 @@ FILE *openUrl(char *url) {
         abort();
     }
     return fp;
+}
+
+indexList newIndexList() {
+    indexList new = calloc(1, sizeof(IndexList));
+    new->head = NULL;
+    new->last = NULL;
+    return new;
+}
+
+void insertIndex(indexList list, char *url, int index) {
+    indexNode new = calloc(1, sizeof(IndexNode));
+    new->url = strdup(url);
+    new->index = index;
+    new->next = NULL;
+    if (list->head == NULL) {
+        list->head = new;
+    } else {
+        list->last->next = new;
+    }
+    list->last = new;
+}
+
+indexList setUpIndex() {
+    indexList list = newIndexList();
+    FILE *fp = fopen("collection.txt", "r");
+    char currURL[MAXURL];
+    int index = 0;
+    while(fscanf(fp, "%s", currURL) == 1) {
+        insertIndex(list, currURL, index);
+        index++;
+    }
+    fclose(fp);
+    return list;
+}
+
+void freeIndexList(indexList list) {
+    indexNode curr = list->head;
+    indexNode prev = NULL;
+    while(curr != NULL) {
+        prev = curr;
+        curr = curr->next;
+        free(prev->url);
+        free(prev);
+    }
+    free(list);
+}
+
+// Change the name of the url into an integer to act as an index
+int NameToNum(indexList list, char *urlName) {
+    indexNode curr = list->head;
+    for (curr = list->head; curr != NULL; curr = curr->next) {
+        if (strcmp(curr->url, urlName) == 0) {
+            return curr->index;
+        }
+    }
+    return 0;
 }
