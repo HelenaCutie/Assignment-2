dcfba7d267c840adf676e8f8854ddf4c4029e7f5
ZmeyGarynich <43955374+ZmeyGarynich@users.noreply.github.com>
2018-10-24 00:48:27 +1100

    Update readData.h

--- readData.h
+++ readData.h
@@ -4,14 +4,32 @@
 #ifndef READDATA_H
 #define READDATA_H
 
-typedef unsigned char Num;
+typedef struct IndexList *indexList;
+typedef struct IndexNode *indexNode;
 
-typedef struct GraphRep *Graph;
+typedef struct IndexNode{
+	char *url;
+    int index;
+    indexNode next;
+} IndexNode;
 
-typedef struct SetRep *Set;
+typedef struct IndexList{
+	indexNode head;
+	indexNode last;
+} IndexList;
 
+// Create a linked list to store the name of urls and corresponding index
+indexList newIndexList();
+// Insert an index node to the given index list
+void insertIndex(indexList list, char *url, int index);
+// Set up an index list using urls in collection.txt
+indexList setUpIndex();
+// Free up the given index list
+void freeIndexList(indexList list);
 // Creates set of urls from collection.txt
 Set urlList();
+// Convert name of an url to the unique index 
+int NameToNum(indexList list, char *urlName);
 // Creates graph of urls from collection.txt
 Graph urlGraph(Set list);
 // Returns pointer to opened url file
